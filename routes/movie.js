597ca731b170a6a5b09a9daf5b7826687545daec
GO20184107 <gonugunta.himasaila@wipro.com>
2021-03-22 20:10:22 +0530

    updated circuit breaker

---
+++ routes/movie.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var service = require("./service/movieService");
+var router = express.Router();
+var request = require('request');
+var axios = require('axios');
+var https = require('https');
+var cb = require('../cb');
+// const agent = new https.Agent({
+//     rejectUnauthorized: false,
+// });
+
+//getting whole api response
+router.get('/', cb, function (req, res, next) {
+    service.wholeResponse(req, res)
+});
+
+//filter by popularity
+router.get('/popularity', cb, function (req, res, next) {
+    service.popularity(req, res)
+});
+
+//filter by date
+router.get('/date', cb, function (req, res, next) {
+    service.date(req, res);
+});
+
+//filter by  genres(using axios.all)
+router.get('/gen', cb, function (req, res, next) {
+    service.genres(req, res);
+});
+
+//filter by using popularity date & gen
+router.get('/filter', cb, function (req, res, next) {
+    service.filter(req, res);
+
+});
+
+
+
+module.exports = router;
